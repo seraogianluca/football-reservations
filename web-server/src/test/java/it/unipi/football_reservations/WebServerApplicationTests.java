32852abf907f91c89b0d74da6c17cb82864491f1
seraogianluca <g.serao@studenti.unipi.it>
2021-09-29 11:39:53 +0200

    spring hellow tutorial

---
+++ web-server/src/test/java/it/unipi/football_reservations/WebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.football_reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
